15a2d64b0a325647bcff0189681412f016f62674
Helen <helencedeo17@gmail.com>
2022-09-24 14:38:38 +0000

    cambié las variables de Contabilización

--- contabiliza.cpp
+++ contabiliza.cpp
@@ -3,18 +3,18 @@
 using namespace std;
 int main()
 {
-	int i=0,l;
-	float x,s=0;
-	cout<<"ingrese el limite l="; cin>>l;
+	int HABC_i=0,HABC_l;
+	float HABC_x,HABC_s=0;
+	cout<<"ingrese el limite HABC_l="; cin>>HABC_l;
 	do{
 
-	cout<<"ingrese el numero x="; cin>>x;
-	i=i+1;
-	s=s+x;
+	cout<<"ingrese el numero HABC_x="; cin>>HABC_x;
+	HABC_i=HABC_i+1;
+	HABC_s=HABC_s+HABC_x;
 
 
 
-	}while(i<l);
-	cout<<"Se ingresaron "<<l<<" numeros "<< "que sumaron "<<s<<endl;
+	}while(HABC_i<HABC_l);
+	cout<<"Se ingresaron "<<HABC_l<<" numeros "<< "que sumaron "<<HABC_s<<endl;
 	return 0;
 }
